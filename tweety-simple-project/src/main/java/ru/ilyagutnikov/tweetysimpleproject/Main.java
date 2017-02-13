8009e57d3b4d385c987764f00fa33947778c168d
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2017-02-13 20:42:33 +0300

    Сделал базовый hello world

--- tweety-simple-project/src/main/java/ru/ilyagutnikov/tweetysimpleproject/Main.java
+++ tweety-simple-project/src/main/java/ru/ilyagutnikov/tweetysimpleproject/Main.java
@@ -1,10 +1,15 @@
 package ru.ilyagutnikov.tweetysimpleproject;
 
+import net.sf.tweety.logics.pl.syntax.Proposition;
+import net.sf.tweety.logics.pl.syntax.PropositionalFormula;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		PropositionalFormula helloWorld = new Proposition("HelloWorld");
+		System.out.println(helloWorld);
+		
 	}
 
 }
